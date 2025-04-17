2b1c96ae27412faf004f530d7e1671c897dc01cc
Diego <diegodelacruz775@gmail.com>
2025-04-17 14:26:50 -0600

    Eliminacion prueba

--- src/tarea_8/Tarea_8.java
+++ src/tarea_8/Tarea_8.java
@@ -8,6 +8,5 @@ public class Tarea_8 {
         FRMventanaPrinical ventana1 = new FRMventanaPrinical();
         ventana1.setVisible(true);
         ventana1.setLocationRelativeTo(null);
-        //Estoy haciendo un comentario de prueba
     }
 }
